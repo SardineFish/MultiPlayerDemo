7652393ecc13d1b2d44dfa5bc8bbb4d18a468858
SardineFish <Sardine@live.cn>
2018-12-13 15:46:52 +0800

    Create project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PlayerController : MonoBehaviour
+{
+    public float MaxSpeed = 10;
+    public float MaxTrunSpeed = 360;
+    public virtual void Move(Vector2 movement)
+    {
+        GetComponent<Rigidbody2D>().MovePosition(transform.position.ToVector2() + movement * MaxSpeed);
+    }
+    public virtual void Aim(Vector2 direction)
+    {
+
+    }
+}
