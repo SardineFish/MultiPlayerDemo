c6fad6bf4d018ba634730a1dbf2b400e7158b357
SardineFish <Sardine@live.cn>
2018-12-16 16:46:37 +0800

    Fix bugs

--- NetworkTestGameServer/GameServer.cs
+++ NetworkTestGameServer/GameServer.cs
@@ -64,24 +64,24 @@ namespace NetworkTestGameServer
                         {
                             Tick = Tick,
                             syncStates = Players.Values
-                        .Select(player => player.GetState())
-                        .Where(state => state != null)
-                        .SelectMany(state => new PlayerState[]
-                            {
-                                state,
-                                Mirror
-                                ? null
-                                :new PlayerState()
-                                {
-                                    ID=Players[Guid.Parse(state.ID)].MirrorID.ToString(),
-                                    Aim=-state.Aim,
-                                    Fire = state.Fire,
-                                    Movement=-state.Movement,
-                                    Position=-state.Position,
-                                    Tick = Tick
-                                }
-                            })
-                        .ToArray()
+                                .Select(player => player.GetState())
+                                .Where(state => state != null)
+                                .SelectMany(state => new PlayerState[]
+                                    {
+                                        state,
+                                        Mirror
+                                            ? new PlayerState()
+                                                {
+                                                    ID=Players[Guid.Parse(state.ID)].MirrorID.ToString(),
+                                                    Aim=-state.Aim,
+                                                    Fire = state.Fire,
+                                                    Movement=-state.Movement,
+                                                    Position=-state.Position,
+                                                    Tick = Tick
+                                                }
+                                            : null
+                                    })
+                                .ToArray()
                         };
                         foreach (var player in Players.Values)
                         {
